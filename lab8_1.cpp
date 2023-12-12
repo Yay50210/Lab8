cac2673f4a2ffda5954fe4851f789b0447d1579c
Best <Photchara_k@cmu.ac.th>
2023-12-12 16:59:25 +0700

    คอมพีท

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -6,12 +6,16 @@ int main()
   char rank;
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
-  }
+  
+    if(rank== 'S') cout << "You have received Super Ultra Rare Unit!!!\n"<< "You have received 5 gems.\n"<< "You have received 1 gems.\n"<<"You have received 2000 coins.\n"<< "You have received very KAK items.\n";
+     
+    if( 'A'==rank) cout << "You have received 5 gems.\n"<< "You have received 1 gems.\n"<<"You have received 2000 coins.\n"<< "You have received very KAK items.\n";
+     
+    if('B'==rank) cout << "You have received 1 gems.\n"<<"You have received 2000 coins.\n"<< "You have received very KAK items.\n";
+    
+    if( 'C'==rank) cout << "You have received 2000 coins.\n"<< "You have received very KAK items.\n";
+    
+    if ('D'==rank) cout << "You have received very KAK items.\n";
+  
   return 0;
 }
--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,46 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    int A, H, B;
+    string C;
+    cout << "Enter your age: ";
+    cin >> A;
+
+    if (A <= 25){
+        cout << "Enter your height: ";
+        cin >> H;
+        if(H<100){
+            C = "Chopper";
+        }else if(H<180){
+            C = "Usopp";
+        }else {
+            cout << "Enter your bounty: ";
+            cin >> B;
+            if(B<=1100000000){
+                C = "Sanji";
+            
+            }else{
+                C = "Zoro";
+            }
+        
+        }
+    }else if(A<=60){
+       cout << "Enter your bounty: ";
+        cin >> B;
+        if(B<=500000000)
+        C = "Franky";
+        else{
+            C = "Jinbe";
+        }
+
+    }else
+    C = "Brook";
+
+
+    cout << "Your character = " << C;
+}
